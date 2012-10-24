9bfc605955e617eebd5a144f7db94270e5462f60
Michal Kapinus <michal.kapinus@gmail.com>
2012-10-24 10:43:26 +0200

    menu aplikace

--- Battle of Endor/src/cz/vutbr/fit/battle/of/endor/MainActivity.java
+++ Battle of Endor/src/cz/vutbr/fit/battle/of/endor/MainActivity.java
@@ -1,8 +1,10 @@
 package cz.vutbr.fit.battle.of.endor;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -16,5 +18,10 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    public void newGame(View view) {
+		Intent intent = new Intent(this, Game.class);
+		startActivity(intent);
     }
 }
